0137e0eed8529921e636ee4e1331e2ad7397efde
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-04-20 17:34:11 -0400

    Part 7 done

--- src/test/java/io/zipcoder/interfaces/TestNewStudents.java
+++ src/test/java/io/zipcoder/interfaces/TestNewStudents.java
@@ -1,5 +1,21 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestNewStudents {
+    @Test
+    public void testStudent(){
+        Students students =new Students();
+        Person person =students.findById(2);
+        Assert.assertTrue(students.contains(person));
+
+    }
+    @Test
+    public void testStudent2(){
+        Students students =new Students();
+        Person person1 =students.findById(3);
+        Assert.assertFalse(students.contains(person1));
+    }
 
 }
